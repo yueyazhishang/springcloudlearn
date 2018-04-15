91bf0f3b735b3d31b81d6a9bb8cfbb35b9918cc9
lizhanxiang <lizx@cloud-young.com>
2018-04-15 16:06:03 +0800

    增加 spring-cloud-consumer-hystrix

---
+++ eureka/spring-cloud-consumer-hystrix/src/main/java/com/lzx/SpringCloudConsumerHystrixApplication.java
@@ -0,0 +1,16 @@
+package com.lzx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+@EnableDiscoveryClient
+public class SpringCloudConsumerHystrixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConsumerHystrixApplication.class, args);
+	}
+}
--- eureka/spring-cloud-consumer-hystrix/src/main/java/com/lzx/client/HelloClient.java
+++ eureka/spring-cloud-consumer-hystrix/src/main/java/com/lzx/client/HelloClient.java
@@ -1,15 +1,22 @@
 package com.example.demo.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * Created by Administrator on 2018/4/5.
  */
-@FeignClient(name= "spring-cloud-producer" )
+@FeignClient(name= "spring-cloud-producer",fallback = HelloRemoteHystrix.class )
 public interface HelloClient {
     @GetMapping("/hello")
     String hello(@RequestParam("name") String name);
-
+}
+@Component
+ class HelloRemoteHystrix implements HelloClient{
+    @Override
+    public String hello(@RequestParam(value = "name") String name) {
+        return "hello" +name+", this messge send failed ";
+    }
 }
